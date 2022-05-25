4da0d54f0c5ef736f37bd2acb45ba54bfd62ffd8
Chirayu Desai <chirayudesai1@gmail.com>
2022-05-25 16:38:47 +0530

    Add firmware extraction support
    Change-Id: I96e5ac8fb2366e9253aabfa6220a3eddf7e4758f

--- extract-files.sh
+++ extract-files.sh
@@ -82,6 +82,8 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
